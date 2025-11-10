ed34f8103422d4e3209dbda58b5bebad0d08a99a
emiliomurithimwaniki <164023207+emiliomurithimwaniki@users.noreply.github.com>
2025-11-10 09:53:48 +0300

    cloudinary

---
+++ public/admin/admin-config.js
@@ -0,0 +1,17 @@
+window.ADMIN_CONFIG = {
+  firebase: {
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
+  },
+  allowedEmails: [
+    "you@example.com"
+  ],
+  cloudinary: {
+    cloud_name: "dmbwesyq4",
+    unsigned_upload_preset: "nasam-hi-tech"
+  }
+};
