cb60940b57c3158ff2aa8f3b0a2a66681db2b8f3
emiliomurithimwaniki <164023207+emiliomurithimwaniki@users.noreply.github.com>
2025-11-10 12:53:05 +0300

    Hero section

---
+++ docs/assets/js/site-config.js
@@ -0,0 +1,9 @@
+window.NASAM_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBE7W_cp4FBvTvjwrGeHQl6KvRtVWVP4a0",
+  authDomain: "nasam-hi-tech.firebaseapp.com",
+  projectId: "nasam-hi-tech",
+  storageBucket: "nasam-hi-tech.firebasestorage.app",
+  messagingSenderId: "1034032702563",
+  appId: "1:1034032702563:web:53674f02ed2562d84e0456",
+  measurementId: "G-WNSH26VPQH"
+};
